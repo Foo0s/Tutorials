bba0f335da7e1bcbeed81a5fa1941adead17c502
Foo0s <oraproavp@list.ru>
2023-01-29 19:30:16 +0300

    Добавление новых элементов.
    Добавил функции.
    Добавил название.
    Добавил иконку.
    Упростил параметры.
    Установил фон.

--- Calture/fontr/window.py
+++ Calture/fontr/window.py
@@ -1,22 +1,45 @@
+#Импортирование библиотек и файлов.
+
 from parameters.params import Params
 import tkinter as tk
 import sys
 
 class Mane_Window:
+    #Установка значений из другого класса.
     def __init__(self):
-        self.screen = Params
+        self.settings = Params()
 
+    #Основное окно GUI-приложения.
+    def dop_main(self):
+        self.root = tk.Tk()
+        self.root.title(self.settings.text())
+        self.root.geometry(self.settings.screen)
+        self.root.resizable(width=False, height=False)
+        self.root.iconbitmap(self.settings.logo())
+        self.root.config(bg=self.settings.color_fon)
+        text = tk.Label(text="123")
+        text.pack()
+        self.root.mainloop()
 
+    #Задаёт цвет.
+    def color_main(self):
+        pass
+
+    #Реакция на выход из приложения.
+    def exit(self):
+        if sys.exit():
+            sys.exit()
+
+    #Запуск программы. Основная функция.
     def main(self):
         while True:
-            root = tk.Tk()
-            greet = tk.Label(text="Hello world!")
-            greet.pack()
-            root.mainloop()
-            if sys.exit():
-                pass
+            self.dop_main()
+            self.root.mainloop()
+            self.exit()
 
 
+
+#Вызов класса.
 if __name__ == "__main__":
     start = Mane_Window()
     start.main()
--- Calture/parameters/params.py
+++ Calture/parameters/params.py
@@ -1,11 +1,19 @@
+#Класс с параметрами для приложения.
 class Params:
     def __init__(self):
-        self.screen_width = 400
-        self.screen_heidth = 350
+        #Размер экрана.
+        self.screen = "350x400"
 
-    def colors(self):
-        self.color_fon = (90, 101, 30)
-        self.color_text = (100, 20, 30)
+        #Цвета.
+        self.color_fon = "yellow"
+        self.color_text = "orange"
 
+    #Функция хранящая в себе название программы
     def text(self):
         self.name_program = "Calculator"
+        return self.name_program
+
+    #Иконки
+    def logo(self):
+        self.icon = "C:/Users/USER/Desktop/Calture/images/icon.ico"
+        return self.icon
